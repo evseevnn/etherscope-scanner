afe08ceafe7d8297f0e9082c4d341379011a3b42
Nikolay Evseev <evseevnn@gmail.com>
2018-08-04 02:46:32 +0700

    add count index on number

--- scripts/updateSchema.js
+++ scripts/updateSchema.js
@@ -11,7 +11,7 @@ const graph = new Graph();
     to.uid: uid @reverse .
     from.address: string @index(hash) @count .
     to.address: string @index(hash) @count .
-    number: int @index(int) @upsert .
+    number: int @index(int) @upsert @count .
     sha3Uncles: string @index(hash) .
     miner: string @index(hash) .
     name: string @index(term) .
