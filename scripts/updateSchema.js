4894880ce2ccc782ca3d20a390a1dc1b87a3a4db
Nikolay Evseev <evseevnn@gmail.com>
2018-08-04 01:41:07 +0700

    fix

--- scripts/updateSchema.js
+++ scripts/updateSchema.js
@@ -5,22 +5,22 @@ const graph = new Graph();
 
 (async () => {
   await graph.setSchema(`
-    _type: string @index(string) .
-    address: string @index(string) .
+    _type: string @index(hash) .
+    address: string @index(hash) .
     from.uid: uid @reverse .
-    from.address: string @index(string) .
+    from.address: string @index(hash) .
     to.uid: uid @reverse .
-    to.address: string @index(string) .
+    to.address: string @index(hash) .
     transactions: uid reverse .
     topics: [string] .
     number: int @index(int) @upsert .
-    sha3Uncles: string @index(string) .
-    miner: string @index(string) .
+    sha3Uncles: string @index(hash) .
+    miner: string @index(hash) .
     name: string @index(term) .
-    symbol: string @index(string) .
-    owner: string @index(string) .
-    contractAddress: string @index(string) .
-    hash: string @index(string) @count .
+    symbol: string @index(hash) .
+    owner: string @index(hash) .
+    contractAddress: string @index(hash) .
+    hash: string @index(hash) @count .
   `)
 
   log(`Schema succeful apply`)
