be9db0e00297fdaae70532da9b442fb8b070c1ed
Nikolay Evseev <evseevnn@gmail.com>
2018-08-03 20:13:51 +0700

    testing speed

--- blockchain/ethereum/listeners/blocks.js
+++ blockchain/ethereum/listeners/blocks.js
@@ -8,7 +8,7 @@ const ethereum = new Ethereum()
 const Graph = require('../../../graphdb')
 const graph = new Graph()
 
-const BLOCKS_PER_TIME = 5
+const BLOCKS_PER_TIME = 1
 
 // Nodes
 const {
@@ -140,7 +140,7 @@ new TasksPool(NEW_BLOCKS_LISTNER)
       Promise.all(promises)
         .then(() => {
           if (from < to) {
-            process.nextTick(() => processing())
+            setImmediate(() => processing())
           } else {
             done()
           }
