24decf47a788b147812f071f550a92e04a794dcf
Nikolay Evseev <evseevnn@gmail.com>
2018-08-07 01:02:59 +0700

    update schema

--- scripts/updateSchema.js
+++ scripts/updateSchema.js
@@ -13,9 +13,6 @@ const graph = new Graph();
     number: int @index(int) @upsert .
     sha3Uncles: string @index(hash) .
     miner: string @index(hash) .
-    name: string @index(term) .
-    symbol: string @index(hash) .
-    owner: string @index(hash) .
     contractAddress: string @index(hash) .
     transactions: uid @reverse .
     contract: uid @reverse .
