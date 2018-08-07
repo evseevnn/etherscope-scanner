79321b4ae9424aeb556d81d52b9b9d1439b604d6
Nikolay Evseev <evseevnn@gmail.com>
2018-08-07 16:27:12 +0700

    fixes

--- db/index.js
+++ db/index.js
@@ -2,9 +2,8 @@ const { MongoClient } = require('mongodb')
 const log = require('debug')
 
 class DB {
-  constructor({ address, database } = { address: process.env.DB_ADDRESS || 'mongodb://localhost:27017', database: process.env.DB_NAME || 'main' }) {
+  constructor({ address } = { address: process.env.DB_ADDRESS || 'mongodb://localhost:27017/main' }) {
     this.address = address
-    this.database = database
   }
 
   async connect() {
@@ -16,7 +15,7 @@ class DB {
         }
         log(`Connect to database successfully`)
         this.client = client
-        resolve(client.db(this.database))
+        resolve(client.db())
       })
     })
   }
