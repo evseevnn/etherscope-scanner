5e86bbe06deb8703b8974e6d305b04ace5ba279c
Nikolay Evseev <evseevnn@gmail.com>
2018-08-11 01:39:13 +0700

    fix

--- db/repositories/AddressesRepository.js
+++ db/repositories/AddressesRepository.js
@@ -1,6 +1,14 @@
 const BaseRepository = require('../BaseRepository')
 
 class AddressesRepository extends BaseRepository {
+  get ADDRESS_TYPE_CONTRACT() {
+    return 'contract'
+  }
+
+  get ADDRESS_TYPE_ACCOUNT() {
+    return 'account'
+  }
+
   get name() {
     return 'addresses'
   }
