8f97a8b3beb15070af934a0f3625fad04baf63aa
Nikolay Evseev <evseevnn@gmail.com>
2018-08-11 01:19:56 +0700

    account grabber

---
+++ scripts/accounts-grabber.js
@@ -0,0 +1,68 @@
+/**
+ * This script will collect all addresses from ethereum db
+ * If address is contract also will saved abi information, properties and available methods
+ */
+require('dotenv').load()
+const log = require('debug')('accounts-grabber')
+const repositories = require('../db/repositories')
+
+repositories
+  .connect()
+  .then(async ({
+    AddressesRepository,
+    TransactionsRepository
+  }) => {
+    let lastProcessedBlock = 0
+    // Getting all creation contracts
+    let addresses = new Map()
+    await TransactionsRepository
+      .find()
+      .sort({ blockNumber: 1, transactionIndex: 1 })
+      .forEach(async transaction => {
+        // Need save it one time per block
+        if (lastProcessedBlock < transaction.blockNumber) {
+          log(`[${lastProcessedBlock}] Save all addresses from block`)
+          const allAccountsAddresses = Array.from(addresses.keys())
+
+          // Get accounts from database for exclude from next ethereum request and saving to db
+          const accountsFromDatabase = await AddressesRepository.find({ address: { $in: allAccountsAddresses } }, { address: 1 }).toArray()
+
+          // Clean saving batch
+          accountsFromDatabase.forEach(account => {
+            addresses.delete(account.address)
+          })
+
+          // Need save data
+          await AddressesRepository.insert(Array.from(addresses.values()))
+          lastProcessedBlock = transaction.blockNumber
+          addresses.clear()
+        }
+
+        // Collect addresses from transaction
+        if (transaction.from) {
+          addresses.push(transaction.from, {
+            address: transaction.from,
+            type: AddressesRepository.ADDRESS_TYPE_ACCOUNT,
+            lastUpdateAt: parseInt(Date.now() / 1000)
+          })
+        }
+
+        if (transaction.to) {
+          addresses.push(transaction.to, {
+            address: transaction.to,
+            type: AddressesRepository.ADDRESS_TYPE_ACCOUNT,
+            lastUpdateAt: parseInt(Date.now() / 1000)
+          })
+        }
+
+        // if transaction has contract creation
+        if (transaction.receipt.contractAddress) {
+          addresses.set(transaction.receipt.contractAddress, {
+            address: transaction.receipt.contractAddress,
+            type: AddressesRepository.ADDRESS_TYPE_CONTRACT,
+            lastUpdateAt: parseInt(Date.now() / 1000)
+          })
+        }
+      })
+  })
+  .catch(log)
