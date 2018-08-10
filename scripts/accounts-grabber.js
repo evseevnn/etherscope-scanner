76e6c6b06b8af4b2a8dd6020eef306af05292c25
Nikolay Evseev <evseevnn@gmail.com>
2018-08-11 02:26:27 +0700

    fixes

--- scripts/accounts-grabber.js
+++ scripts/accounts-grabber.js
@@ -14,7 +14,7 @@ repositories
   }) => {
     let lastProcessedBlock = 0
     // Getting all creation contracts
-    const addresses = new Map()
+    let addresses = {}
     await TransactionsRepository
       .find()
       .sort({ blockNumber: 1 })
@@ -25,22 +25,21 @@ repositories
         log(`[#${transaction.blockNumber}] Getting addresses from block`)
         // Need save it one time per block
         if (lastProcessedBlock < transaction.blockNumber) {
-          const allAccountsAddresses = Array.from(addresses.keys())
+          const allAccountsAddresses = Object.keys(addresses)
 
-          console.log('before: ', addresses)
           // Get accounts from database for exclude from next ethereum request and saving to db
           const accountsFromDatabase = await AddressesRepository.find({ address: { $in: allAccountsAddresses } }, { address: 1 }).toArray()
           // Clean saving batch
           accountsFromDatabase.forEach(account => {
-            addresses.delete(account.address)
+            delete addresses[account.address]
           })
-          console.log('after: ', addresses)
-          console.log('as array: ', Array.from(addresses.values()))
+
           // Need save data
-          if (addresses.size) {
-            log(`[#${transaction.blockNumber}] Trying save ${addresses.size} addresses`)
-            await AddressesRepository.insert(Array.from(addresses.values()))
-            addresses.clear()
+          const addressesData = Object.values(addresses.values())
+          if (addressesData.length) {
+            log(`[#${transaction.blockNumber}] Trying save ${addressesData.length} addresses`)
+            await AddressesRepository.insert(addressesData)
+            addresses = {}
           }
           lastProcessedBlock = transaction.blockNumber
           log(`[${lastProcessedBlock}] Saved`)
@@ -48,28 +47,28 @@ repositories
 
         // Collect addresses from transaction
         if (transaction.from) {
-          addresses.set(transaction.from, {
+          addresses[transaction.from] = {
             address: transaction.from,
             type: AddressesRepository.ADDRESS_TYPE_ACCOUNT,
             lastUpdateAt: parseInt(Date.now() / 1000)
-          })
+          }
         }
 
         if (transaction.to) {
-          addresses.set(transaction.to, {
+          addresses[transaction.to] = {
             address: transaction.to,
             type: AddressesRepository.ADDRESS_TYPE_ACCOUNT,
             lastUpdateAt: parseInt(Date.now() / 1000)
-          })
+          }
         }
 
         // if transaction has contract creation
         if (transaction.receipt.contractAddress) {
-          addresses.set(transaction.receipt.contractAddress, {
+          addresses[transaction.receipt.contractAddress] = {
             address: transaction.receipt.contractAddress,
             type: AddressesRepository.ADDRESS_TYPE_CONTRACT,
             lastUpdateAt: parseInt(Date.now() / 1000)
-          })
+          }
         }
       })
   })
