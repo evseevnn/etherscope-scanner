3b9e0af5b1b013ccc9486be3ed850d79e5c19de1
Nikolay Evseev <evseevnn@gmail.com>
2018-09-14 11:09:04 +0700

    checing

--- ethereum/listeners/blocks.js
+++ ethereum/listeners/blocks.js
@@ -39,14 +39,14 @@ repositories
                 const addressesForSave = []
                 for (let i = 0; i < transactionsWithContracts.length; i++) {
                   const transactionWithContract = transactionsWithContracts[i]
-                  const opcode = ethereum.getContractOpcode(transactionWithContract.receipt.contractAddress)
-                  const interfaces = ethereum.getContractInterfaces(transactionWithContract.receipt.contractAddress, opcode)
-                  const data = await ethereum.getContractDataByInterfaces(transactionWithContract.receipt.contractAddress, interfaces)
-                  log(`[#${blockNumber}][${transactionWithContract.receipt.contractAddress}] interfaces ${interfaces.join(', ')}`)
+                  // const opcode = ethereum.getContractOpcode(transactionWithContract.receipt.contractAddress)
+                  // const interfaces = ethereum.getContractInterfaces(transactionWithContract.receipt.contractAddress, opcode)
+                  // const data = await ethereum.getContractDataByInterfaces(transactionWithContract.receipt.contractAddress, interfaces)
+                  // log(`[#${blockNumber}][${transactionWithContract.receipt.contractAddress}] interfaces ${interfaces.join(', ')}`)
                   addressesForSave.push({
-                    instanceOf: interfaces,
-                    data,
-                    opcode,
+                    // instanceOf: interfaces,
+                    // data,
+                    // opcode,
                     address: transactionWithContract.receipt.contractAddress,
                     type: AddressesRepository.ADDRESS_TYPE_CONTRACT,
                     createdAt: new Date(block.timestamp * 1000)
