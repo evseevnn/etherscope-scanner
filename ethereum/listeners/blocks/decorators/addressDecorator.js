3bf9f6b2b508299edf423fb643334a91e9b3f18a
Nikolay Evseev <evseevnn@gmail.com>
2018-09-22 03:41:28 +0700

    contract prepare fix

--- ethereum/listeners/blocks/decorators/addressDecorator.js
+++ ethereum/listeners/blocks/decorators/addressDecorator.js
@@ -8,7 +8,7 @@ module.exports = async (address, AddressesRepository, lightMode = false) => {
     if (addressData) {
       switch (addressData.type) {
         case 'contract':
-          if (addressData.data && addressData.data.totalSupply) {
+          if (addressData.data && addressData.data.totalSupply && addressData.data.decimals) {
             addressData.data.totalSupply = (addressData.data.totalSupply / (Math.pow(10, addressData.data.decimals) || 1).toFixed(8).replace(/\.?0+$/, ''))
           }
           const { address, type, data, instanceOf } = addressData
