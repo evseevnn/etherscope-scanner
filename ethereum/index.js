deb180f2124813b9603747de724b08b2ecb58299
Nikolay Evseev <evseevnn@gmail.com>
2018-10-05 11:27:08 +0700

    merge and remove temporary changes

--- ethereum/index.js
+++ ethereum/index.js
@@ -134,7 +134,6 @@ class Ethereum extends EventEmitter {
    * @param {String} address
    */
   getContractOpcode(address) {
-    throw Error('Why?')
     try {
       const opcode = exec.execSync(`myth -d -a "${address}" --rpc=${process.env.ETHEREUM_NODE_RPC}`).toString()
       if (opcode.startsWith('Received an empty response')) {
