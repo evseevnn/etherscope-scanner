99840b235f3c9abdcfc572e20b0ec6b0e745ce30
Nikolay Evseev <evseevnn@gmail.com>
2018-10-05 11:24:19 +0700

    remove unused repo

--- db/repositories/AddressesRepository.1.js
+++
@@ -1,9 +0,0 @@
-const BaseRepository = require('../BaseRepository')
-
-class AddressesRepository extends BaseRepository {
-  get name() {
-    return 'contracts'
-  }
-}
-
-module.exports = AddressesRepository
