fa9444cada96ab1092e391902afd811f31cde154
Nikolay Evseev <evseevnn@gmail.com>
2018-10-19 18:54:38 +0700

    log improve

--- ethereum/workers/blocks/index.js
+++ ethereum/workers/blocks/index.js
@@ -25,16 +25,16 @@ Promise.all([
 }]) => {
   new TasksPool(process.env.CATCHING_UP_MODE ? CATCHING_UP_BLOCKS_LISTNER : NEW_BLOCKS_LISTNER)
   .connectAsReader('blocks', async ({ blockNumber }, done) => {
-    log(`[#${blockNumber}] Start processing block`)
+    log(`[${blockNumber}] Start processing block`)
 
     const [ isBlockExist ] = await BlocksReposiroty.find({ number: blockNumber }).limit(1).toArray()
 
     if (!isBlockExist) {
       try {
         // Getting all block data
-        log(`[#${blockNumber}] Getting block data`)
+        log(`[${blockNumber}] Getting block data`)
         let { block, transactions } = await ethereum.getBlockData(blockNumber)
-        log(`[#${blockNumber}] Prepare and save`)
+        log(`[${blockNumber}] Prepare and save`)
 
         if (transactions.length) {
           // Get exists transactions for block
@@ -146,7 +146,7 @@ Promise.all([
           // Save last transactions
           await TransactionsRepository.update(transactions, ['hash'], true)
 
-          log(`[#${blockNumber}] Send ${transactions.length} transactions to processing`)
+          log(`[${blockNumber}] Send ${transactions.length} transactions to processing`)
           for (let i = 0; i < transactions.length; i++) {
             await contractsProcessingPool.send({ hash: transactions[i].hash })
           }
@@ -157,18 +157,18 @@ Promise.all([
         // Save block at last
         await BlocksReposiroty.insert(block)
 
-        log(`[#${blockNumber}] Done (tx=${transactions.length})`)
+        log(`[${blockNumber}] Done (tx=${transactions.length})`)
 
         if (global.gc) {
           global.gc()
         }
         setImmediate(() => done())
       } catch (error) {
-        log(`[#${blockNumber}] processing error`, error)
+        log(`[${blockNumber}] processing error`, error)
         process.exit()
       }
     } else {
-      log(`[#${blockNumber}] Exist`)
+      log(`[${blockNumber}] Exist`)
       done()
     }
   })
