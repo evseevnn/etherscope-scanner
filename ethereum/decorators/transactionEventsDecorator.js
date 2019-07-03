40bcff901fbe9485f064a2e489517b5dc9f9446c
Nikolay Evseev <nikolay.evseev@expay.asia>
2019-07-03 08:43:03 +0300

    fix

--- ethereum/decorators/transactionEventsDecorator.js
+++ ethereum/decorators/transactionEventsDecorator.js
@@ -15,42 +15,44 @@ module.exports = async (logs, AddressesRepository) => {
     const address = await addressDecorator(log.address, AddressesRepository)
     if (address.type === 'contract') {
       const eventHash = log.topics && log.topics[0] // Take event hash
-      const event = events[eventHash.substring(2)]
-      if (event && log.data !== '0x') {
-        try {
-          // Checking abi input
-          let eventsInputs = event.inputs
-          const indexedInput = eventsInputs.filter(input => input.indexed)
-          const topicsData = log.topics.slice(1)
-          if (indexedInput.length > topicsData.length) {
-            eventsInputs = eventsInputs.map(input => {
-              input.indexed = false
-              return input
-            })
-          }
-          const data = Object.assign({}, AbiCoder().decodeLog(eventsInputs, log.data, topicsData))
-          const clearEventData = cleanWeb4DecodedFields(data, true)
-          if (['Transfer', 'Approval'].includes(event.name) && clearEventData && clearEventData.value) {
-            clearEventData.value = (clearEventData.value / (Math.pow(10, address.data.decimals) || 1)).toFixed(8).replace(/\.?0+$/, '')
-          }
-
-          const fields = Object.keys(clearEventData)
-          for (let i = 0; i < fields.length; i++) {
-            if (isAddress.test(clearEventData[fields[i]])) {
-              clearEventData[fields[i]] = await addressDecorator(clearEventData[fields[i]], AddressesRepository, true)
+      if (eventHash) {
+        const event = events[eventHash.substring(2)]
+        if (event && log.data !== '0x') {
+          try {
+            // Checking abi input
+            let eventsInputs = event.inputs
+            const indexedInput = eventsInputs.filter(input => input.indexed)
+            const topicsData = log.topics.slice(1)
+            if (indexedInput.length > topicsData.length) {
+              eventsInputs = eventsInputs.map(input => {
+                input.indexed = false
+                return input
+              })
             }
+            const data = Object.assign({}, AbiCoder().decodeLog(eventsInputs, log.data, topicsData))
+            const clearEventData = cleanWeb4DecodedFields(data, true)
+            if (['Transfer', 'Approval'].includes(event.name) && clearEventData && clearEventData.value) {
+              clearEventData.value = (clearEventData.value / (Math.pow(10, address.data.decimals) || 1)).toFixed(8).replace(/\.?0+$/, '')
+            }
+  
+            const fields = Object.keys(clearEventData)
+            for (let i = 0; i < fields.length; i++) {
+              if (isAddress.test(clearEventData[fields[i]])) {
+                clearEventData[fields[i]] = await addressDecorator(clearEventData[fields[i]], AddressesRepository, true)
+              }
+            }
+  
+            decoratedEvents.push({
+              index: log.logIndex,
+              address: (({ address, type, data, instanceOf }) => ({ address, type, data, instanceOf }))(address),
+              name: event.name,
+              code: event._signature,
+              data: clearEventData
+            })
+            // logger(`Event ${events[eventHash].name}`)
+          } catch (error) {
+            logger('Cannot decode events', error, {inputs: event.inputs, data: log.data, topics: log.topics})
           }
-
-          decoratedEvents.push({
-            index: log.logIndex,
-            address: (({ address, type, data, instanceOf }) => ({ address, type, data, instanceOf }))(address),
-            name: event.name,
-            code: event._signature,
-            data: clearEventData
-          })
-          // logger(`Event ${events[eventHash].name}`)
-        } catch (error) {
-          logger('Cannot decode events', error, {inputs: event.inputs, data: log.data, topics: log.topics})
         }
       }
     }
